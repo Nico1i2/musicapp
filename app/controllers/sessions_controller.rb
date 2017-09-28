c7af7a69dc55dd5fdedee58aa804718dc01e275f
nico1i2 <nico1i2@hotmail.com>
2017-09-27 21:23:49 -0500

    revision new sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,38 @@
+class SessionsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:new, :create]
+
+  def new
+  end
+
+  def logged_in
+    user = User.find_by(email: session_params[:email])
+    if user and user.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Logged in"
+      @current_user = user
+      redirect_to main_path
+    else
+      flash[:error] = "invalid credentials"
+      redirect_to new_session_path
+    end
+  end
+
+  def destroy
+    binding.pry
+    session.delete(:user_id)
+    flash[:success] = "Logout successfully"
+    redirect_to new_session_path
+  end
+
+  def main
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+
+
+end
---
+++ db/schema.rb
@@ -0,0 +1,58 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170927025153) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string "album_name"
+    t.string "release"
+    t.string "recorded"
+    t.string "studio"
+    t.string "genre"
+    t.string "label"
+    t.string "producer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string "description"
+    t.string "origin"
+    t.string "website"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "playlist_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "song_name"
+    t.integer "song_length"
+    t.string "songwriter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "password"
+    t.string "user_name"
+    t.string "gender"
+    t.string "birth_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
